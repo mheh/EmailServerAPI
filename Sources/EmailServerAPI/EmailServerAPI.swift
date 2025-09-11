b8e94a4813dd44c61329c5e61921e5a629ce6c87
Milo Hehmsoth <mhehmsoth@gmail.com>
2025-09-11 15:28:50 -0500

    Copy servers

--- Sources/EmailServerAPI/EmailServerAPI.swift
+++ Sources/EmailServerAPI/EmailServerAPI.swift
@@ -9,32 +9,7 @@ import Foundation
 import OpenAPIRuntime
 import HTTPTypes
 
-/// Insert the username and password as basic auth
-public struct BasicAuthMiddleware: ClientMiddleware {
-    let base64String: String
-    
-    public init(base64String: String) {
-        self.base64String = base64String
-    }
-    
-    public func intercept(
-        _ request: HTTPRequest,
-        body: HTTPBody?,
-        baseURL: URL,
-        operationID: String,
-        next: (HTTPRequest, HTTPBody?, URL) async throws -> (HTTPResponse, HTTPBody?)
-    ) async throws -> (HTTPResponse, HTTPBody?) {
-        var headers = request.headerFields
-        headers.append(.init(name: .authorization, value: "Basic \(self.base64String)"))
-        let newRequest: HTTPRequest = .init(
-            method: request.method,
-            scheme: request.scheme,
-            authority: request.authority,
-            path: request.path,
-            headerFields: headers)
-        return try await next(newRequest, body, baseURL)
-    }
-}
+// MARK: Client Streaming
 
 public actor ClientStream {
     public typealias StreamOutput = AsyncStream<EmailServerAPI.Components.Schemas.SMTPServerStreamInput>
@@ -86,3 +61,70 @@ public actor ClientStream {
         }
     }
 }
+
+// MARK: Server Stream Storage
+public actor StreamStorage: Sendable {
+    private typealias StreamType = AsyncStream<Components.Schemas.SMTPServerStreamInput>
+    private var streams: [String: Task<Void, any Error>] = [:]
+    
+    public init() {}
+    
+    private func finishedStream(id: String) {
+        print("Finishing stream \(id)")
+        guard self.streams[id] != nil else { return }
+        self.streams.removeValue(forKey: id)
+        print("Freed up stream \(id)")
+    }
+    private func cancelStream(id: String) {
+        print("Cancelling stream \(id)")
+        guard let task = self.streams[id] else { return }
+        self.streams.removeValue(forKey: id)
+        task.cancel()
+        print("Canceled stream \(id)")
+    }
+    
+    public func makeStream(input: Operations.SmtpStream.Input) -> AsyncStream<Components.Schemas.SMTPServerStreamInput> {
+        let name = input.query.smtpHost
+        let id = UUID().uuidString
+        print("Creating stream \(id) for name: \(name)")
+        let (stream, continuation) = StreamType.makeStream()
+        continuation.onTermination = { termination in
+            Task { [weak self] in
+                switch termination {
+                case .cancelled: await self?.cancelStream(id: id)
+                case .finished: await self?.finishedStream(id: id)
+                @unknown default: await self?.finishedStream(id: id)
+                }
+            }
+        }
+        let inputStream =
+            switch input.body {
+            case .applicationJsonl(let body): body.asDecodedJSONLines(of: Components.Schemas.SMTPServerStreamInput.self)
+            }
+        let task = Task<Void, any Error> {
+            for try await message in inputStream {
+                try Task.checkCancellation()
+                switch message.input {
+                case .SimpleSMTPEmail(let email):
+                    print("Received \(email)")
+                case .SMTPLogin(let login):
+                    print("Received \(login)")
+                case .SMTPLogout(let logout):
+                    print("Received \(logout)")
+                case .StreamKeepAlive(let keepAlive):
+                    print("Recieved \(keepAlive)")
+                    guard keepAlive.keepAlive == true else {
+                        continuation.finish()
+                        break
+                    }
+                    let keepAlive: Components.Schemas.SMTPServerStreamInput = .init(input: .StreamKeepAlive(.init(keepAlive: true)))
+                    continuation.yield(keepAlive)
+                }
+            }
+            continuation.finish()
+        }
+        self.streams[id] = task
+        return stream
+    }
+}
+
