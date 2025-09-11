2f23fb1ab1a71b645ba50ebbffde2004b40c19a9
Milo Hehmsoth <mhehmsoth@gmail.com>
2025-09-11 15:06:52 -0500

    Duplicate email servers for testing

--- Sources/EmailServerAPI/EmailServerAPI.swift
+++ Sources/EmailServerAPI/EmailServerAPI.swift
@@ -35,3 +35,54 @@ public struct BasicAuthMiddleware: ClientMiddleware {
         return try await next(newRequest, body, baseURL)
     }
 }
+
+public actor ClientStream {
+    public typealias StreamOutput = AsyncStream<EmailServerAPI.Components.Schemas.SMTPServerStreamInput>
+    
+    let output: StreamOutput
+    let continuation: StreamOutput.Continuation
+    
+    public init(output: StreamOutput, continuation: StreamOutput.Continuation) async throws {
+        self.output = output
+        self.continuation = continuation
+        try await Self.createTaskGroup(output: self.output, continuation: continuation)
+    }
+    
+    public func handle(_ incoming: EmailServerAPI.Components.Schemas.SMTPServerStreamInput) async {
+        print("handling message: \(incoming)")
+    }
+    
+    public func send(_ outgoing: EmailServerAPI.Components.Schemas.SMTPServerStreamInput) {
+        print("sending message: \(outgoing)")
+    }
+    
+    static func createTaskGroup(output: StreamOutput, continuation: StreamOutput.Continuation) async throws {
+        try await withThrowingTaskGroup(of: Void.self) { group in
+            group.addTask {
+                for try await message in output {
+                    switch message.input {
+                    case .SimpleSMTPEmail(let email):
+                        print("Received \(email)")
+                    case .SMTPLogin(let login):
+                        print("Received \(login)")
+                    case .SMTPLogout(let logout):
+                        print("Received \(logout)")
+                    case .StreamKeepAlive(let keepAlive):
+                        print("Recieved \(keepAlive)")
+                    }
+                    continuation.yield(message)
+                }
+            }
+            
+            group.addTask {
+                for _ in 0...100 {
+                    try await Task.sleep(for: .seconds(2))
+                    let keepAlive: Components.Schemas.SMTPServerStreamInput = .init(input: .StreamKeepAlive(.init(keepAlive: true)))
+                    continuation.yield(keepAlive)
+                }
+            }
+            
+            return try await group.waitForAll()
+        }
+    }
+}
--- Tests/EmailServerAPI-Tests/ClientTests.swift
+++ Tests/EmailServerAPI-Tests/ClientTests.swift
@@ -8,13 +8,20 @@
 import Testing
 import EmailServerAPI
 import OpenAPIAsyncHTTPClient
+import OpenAPIRuntime
+import Foundation
+import HTTPTypes
 
 @Suite("Client tests")
 struct ClientTests {
     
     @Test("Client connect")
     func clientConnect() async throws {
-        let client = Client(serverURL: .init(string: "http://127.0.0.1:8080")!, transport: AsyncHTTPClientTransport())
+        let client = Client(
+            serverURL: .init(string: "http://127.0.0.1:8080")!,
+            transport: AsyncHTTPClientTransport(),
+            middlewares: [LoggingMiddleware()]
+        )
         
         // form the client output stream
         let (outStream, continuation) = AsyncStream<EmailServerAPI.Components.Schemas.SMTPServerStreamInput>.makeStream()
@@ -23,46 +30,45 @@ struct ClientTests {
             .init(outStream.asEncodedJSONLines(), length: .unknown, iterationBehavior: .single)
         )
         
-        let response = try await client.smtpStream(
-            query: .init(smtpHost: "", smtpHostPort: 465),
-            headers: .init(connection: "keep-alive"),
-            body: requestBody)
-        let serverStream = try response.ok.body.applicationJsonl.asDecodedJSONLines(of: String.self)
-        
-        try await withThrowingTaskGroup(of: Void.self) { group in
-            // Listen for upcoming messages
-            group.addTask {
-                for try await message in serverStream {
-                    try Task.checkCancellation()
-                    print("Got greeting: \(message)")
-                }
-            }
-        
-            // dummy messages
-            group.addTask {
-                for _ in 0...4 {
-                    print("Sending a dummy message...")
-//                    try await Task.sleep(nanoseconds: 1 * 1_000_000)
-                    let yielding = EmailServerAPI.Components.Schemas.SMTPServerStreamInput.InputPayload.SMTPLogin(.init(username: "", password: ""))
-                    continuation.yield(.init(input: yielding))
-                    print("Sent a dummy message")
-                }
-                try Task.checkCancellation()
-            }
+        do {
+            let req = try await client.smtpStream(
+                query: .init(smtpHost: "asdf", smtpHostPort: 465),
+                headers: .init(connection: "keep-alive"),
+                body: requestBody)
             
-            // Send messages
-            group.addTask {
-                try Task.checkCancellation()
-                for try await output in outStream {
-                    print("Sending output: \(output)")
-                    continuation.yield(output)
-                }
-//                try await Task.sleep(nanoseconds: 1 * 1_000_000_000)
-                continuation.finish()
+            let response = try req.ok.body.applicationJsonl.asDecodedJSONLines(of: EmailServerAPI.Components.Schemas.SMTPServerStreamInput.self)
+            let streamer = try await ClientStream(output: outStream, continuation: continuation)
+            for try await message in response {
+                await streamer.handle(message)
             }
-            return try await group.waitForAll()
+        } catch {
+            print(error)
+        }
+    }
+}
+
+struct LoggingMiddleware: ClientMiddleware {
+    func intercept(
+        _ request: HTTPTypes.HTTPRequest, body: OpenAPIRuntime.HTTPBody?, baseURL: URL, operationID: String,
+        next: @Sendable (HTTPTypes.HTTPRequest, OpenAPIRuntime.HTTPBody?, URL) async throws -> (HTTPTypes.HTTPResponse, OpenAPIRuntime.HTTPBody?)
+    ) async throws -> (HTTPTypes.HTTPResponse, OpenAPIRuntime.HTTPBody?) {
+        
+        print(request.headerFields)
+        print(request.path ?? "none")
+        let copy = body
+        
+        if let body {
+            switch body.length {
+            case .known(let length):
+                let bodyData = try await Data(collecting: body, upTo: Int(length))
+                if let string = String(data: bodyData, encoding: .utf8) {
+                    print(string)
+                }
+            case .unknown:
+                break
+            }
         }
         
-        
+        return try await next(request, copy, baseURL)
     }
 }
