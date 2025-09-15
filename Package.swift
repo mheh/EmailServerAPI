80507c6595e619c3d66640b347e2420dc5dae567
Milo Hehmsoth <mhehmsoth@gmail.com>
2025-09-14 20:44:55 -0500

    Start from scratch for websocket build

--- Package.swift
+++ Package.swift
@@ -17,24 +17,18 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Cocoanetics/SwiftMail", revision: "1a5f874"),
-        .package(url: "https://github.com/apple/swift-openapi-generator.git", exact: "1.10.2"),
-        .package(url: "https://github.com/apple/swift-openapi-runtime.git", exact: "1.8.2"),
-        .package(url: "https://github.com/swift-server/swift-openapi-async-http-client", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "EmailServerAPI",
             dependencies: [
                 .product(name: "SwiftMail", package: "SwiftMail"),
-                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime")
             ],
-            plugins: [ .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator") ]
         ),
         .testTarget(
             name: "EmailServerAPI-Tests",
             dependencies: [
                 .target(name: "EmailServerAPI"),
-                .product(name: "OpenAPIAsyncHTTPClient", package: "swift-openapi-async-http-client"),
             ]
         )
     ]
--- Sources/EmailServerAPI/Handler.swift
+++ Sources/EmailServerAPI/Handler.swift
@@ -6,59 +6,10 @@
 //
 
 import Foundation
-import OpenAPIRuntime
-import HTTPTypes
 
 // MARK: Shared Handler
-public struct Handler: APIProtocol {
-    private let storage: SMTPStreamStorage = .init()
+public struct Handler {
     
     public init () {}
     
-    public func smtpStream(_ input: EmailServerAPI.Operations.SmtpStream.Input) async throws -> EmailServerAPI.Operations.SmtpStream.Output {
-        do {
-            let eventStream = try await self.storage.make(input: input, id: UUID())
-            
-            let responseBody = Operations.SmtpStream.Output.Ok.Body.applicationJsonl(
-                .init(eventStream.stream.asEncodedJSONLines(), length: .unknown, iterationBehavior: .single)
-            )
-            
-            return .ok(.init(body: responseBody))
-        } catch {
-            print(error)
-            return .internalServerError
-        }
-    }
-    
-    public func smtpActiveConnections(_ input: Operations.SmtpActiveConnections.Input) async throws -> Operations.SmtpActiveConnections.Output {
-        return await .ok(.init(body: .json(self.storage.allConnections())))
-    }
-    
-    public func smtpLogin(_ input: Operations.SmtpLogin.Input) async throws -> Operations.SmtpLogin.Output {
-        guard let uuid = UUID.init(uuidString: input.path.smtpConnectionId) else {
-            throw BodyError.badPathId(id: input.path.smtpConnectionId)
-        }
-        guard case .json(let body) = input.body else { throw BodyError.cannotDecodeBody }
-        let streamState = try await self.storage.login(id: uuid, username: body.username, password: body.password)
-        return .ok(.init(body: .json(streamState)))
-    }
-    
-    public func smtpLogout(_ input: Operations.SmtpLogout.Input) async throws -> Operations.SmtpLogout.Output {
-        guard let uuid = UUID.init(uuidString: input.path.smtpConnectionId) else {
-            throw BodyError.badPathId(id: input.path.smtpConnectionId)
-        }
-        let streamState = try await self.storage.logout(id: uuid)
-        return .ok(.init(body: .json(streamState)))
-    }
-    
-    public func imapStream(_ input: Operations.ImapStream.Input) async throws -> Operations.ImapStream.Output {
-        return .internalServerError
-    }
-}
-
-extension Handler {
-    enum BodyError: Error {
-        case badPathId(id: String)
-        case cannotDecodeBody
-    }
 }
